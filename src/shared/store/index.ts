05e2b89856cf1795c45e98f074633ee713ddbaf8
Bryson G <114206517+bryson-g@users.noreply.github.com>
2025-07-03 11:00:00 -0700

    Set up Reflex store architecture

---
+++ src/shared/store/index.ts
@@ -0,0 +1,10 @@
+import { CombineStates } from "@rbxts/reflex";
+import { playersSlice } from "./slices/players";
+import { clientSlice } from "./slices/client";
+
+export type SharedState = CombineStates<typeof slices>;
+
+export const slices = {
+	players: playersSlice,
+	client: clientSlice,
+};
