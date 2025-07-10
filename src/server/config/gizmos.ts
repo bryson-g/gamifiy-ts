bb7984cce9da77c7af749d1c51d9ede3573e284b
Bryson G <114206517+bryson-g@users.noreply.github.com>
2025-07-10 14:30:00 -0700

    Add gizmo configuration

---
+++ src/server/config/gizmos.ts
@@ -0,0 +1,9 @@
+// import { ServerStorage } from "@rbxts/services";
+// import { GizmoProps } from "server/classes/Gizmo";
+
+// export const gizmos = {
+// 	exampleGizmo: {
+// 		name: "Example Gizmo",
+// 		tool: ServerStorage.Assets.Gizmos.ExampleGizmo,
+// 	},
+// } as const satisfies Record<string, Omit<GizmoProps, "owner">>;
