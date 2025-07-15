8c1e9b487afe77a12b9643872db9da0f276fe6ad
Bryson G <114206517+bryson-g@users.noreply.github.com>
2025-07-15 14:15:00 -0700

    Add money type definitions

---
+++ types/Money.d.ts
@@ -0,0 +1,7 @@
+type Money = Model & {
+	Collision: Part & {
+		BillboardGui: BillboardGui & {
+			TextLabel: TextLabel;
+		};
+	};
+}
---
+++ types/MoneyBGUI.d.ts
@@ -0,0 +1,3 @@
+type MoneyBGUI = BillboardGui & {
+	TextLabel: TextLabel;
+}
