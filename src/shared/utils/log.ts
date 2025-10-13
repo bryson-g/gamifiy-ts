8f72d3a5ad5ad03bd2ad6798dc5972c0167b369c
Bryson G <114206517+bryson-g@users.noreply.github.com>
2025-10-13 14:20:00 -0700

    Extend logging utilities with convenience methods

--- src/shared/utils/log.ts
+++ src/shared/utils/log.ts
@@ -1,1 +1,19 @@
 import Log, { Logger } from "@rbxts/log";
+
+export const logger = Log.ForContext("rbxts-dev");
+
+export function logDebug(message: string, ...args: unknown[]) {
+	logger.Debug(message, ...args);
+}
+
+export function logInfo(message: string, ...args: unknown[]) {
+	logger.Info(message, ...args);
+}
+
+export function logWarning(message: string, ...args: unknown[]) {
+	logger.Warn(message, ...args);
+}
+
+export function logError(message: string, ...args: unknown[]) {
+	logger.Error(message, ...args);
+}
