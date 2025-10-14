a5333a715b62b5902f52921fa65d8be07544009c
Bryson G <114206517+bryson-g@users.noreply.github.com>
2025-10-14 10:30:00 -0700

    Define shared constants for configuration

---
+++ src/shared/constants.ts
@@ -0,0 +1,12 @@
+export const MAX_PLAYERS = 50;
+export const MIN_PLAYERS = 2;
+
+export const DEFAULT_CURRENCY = 0;
+export const DEFAULT_XP = 0;
+
+export const XP_PER_LEVEL = 100;
+export const XP_MULTIPLIER = 1.5;
+
+export const CURRENCY_REWARD_BASE = 10;
+export const CURRENCY_REWARD_MULTIPLIER = 2;
+
